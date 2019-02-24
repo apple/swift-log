7db09d7d68737d1c3a4a5a6f8c8b89c6637a6b9b
Johannes Weiss <johannesweiss@apple.com>
2019-02-24 13:51:38 +0000

    add license information (#48)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,17 @@
 // swift-tools-version:4.2
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Logging API open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Logging API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Logging API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
--- Tests/LoggingTests/LocalLoggingTest.swift
+++ Tests/LoggingTests/LocalLoggingTest.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Logging API open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Logging API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Logging API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 @testable import Logging
 import XCTest
 
