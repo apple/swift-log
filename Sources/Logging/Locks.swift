9a393a5d351d4dd23440f713bf22c3b7f6b6643c
tomer doron <tomer@apple.com>
2019-04-11 06:12:41 -0700

    retain SwiftNIO copyright header (#44)
    motivation: retain copyrights
    
    changes:
    * add notice file with references to derived work
    * add swift-nio license header to lock

--- Sources/Logging/Locks.swift
+++ Sources/Logging/Locks.swift
@@ -11,6 +11,21 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
     import Darwin
 #else
