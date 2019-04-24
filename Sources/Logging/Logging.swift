4ac1139ca1bc3aab8db582eb30654d86e434e1e7
Yim Lee <yim_lee@apple.com>
2019-04-24 13:15:43 -0700

    Fix API documentation
    Motivation:
    
    API doc contains misinformation.
    
    Modifications:
    
    Update documentation for `Logger` API to match code.
    
    Result:
    
    API doc is correct.

--- Sources/Logging/Logging.swift
+++ Sources/Logging/Logging.swift
@@ -97,7 +97,7 @@ extension Logger {
 }
 
 extension Logger {
-    /// Log a message passing with the `Logger.trace` log level.
+    /// Log a message passing with the `Logger.Level.trace` log level.
     ///
     /// If `.trace` is at least as severe as the `Logger`'s `logLevel`, it will be logged,
     /// otherwise nothing will happen.
@@ -119,7 +119,7 @@ extension Logger {
         self.log(level: .trace, message(), metadata: metadata(), file: file, function: function, line: line)
     }
 
-    /// Log a message passing with the `Logger.info` log level.
+    /// Log a message passing with the `Logger.Level.debug` log level.
     ///
     /// If `.debug` is at least as severe as the `Logger`'s `logLevel`, it will be logged,
     /// otherwise nothing will happen.
