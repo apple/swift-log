eed8d6ec08e272ca65bf32b028203adcaa485cfb
Johannes Weiss <johannesweiss@apple.com>
2020-03-13 13:00:12 +0000

    test that demonstrates overloading
    Motivation:
    
    SwiftLog is meant as a base logging library so users will often overload
    certain functions with their own specialised and opinionated overloads.
    
    Modification:
    
    Add a test as demonstration.
    
    Result:
    
    More tests.

--- Tests/LoggingTests/LoggingTest.swift
+++ Tests/LoggingTests/LoggingTest.swift
@@ -550,4 +550,29 @@ class LoggingTest: XCTestCase {
 
         fds.forEach { close($0) }
     }
+
+    func testOverloadingError() {
+        struct Dummy: Error, LocalizedError {
+            var errorDescription: String? {
+                return "errorDescription"
+            }
+        }
+        // bootstrap with our test logging impl
+        let logging = TestLogging()
+        LoggingSystem.bootstrapInternal(logging.make)
+
+        var logger = Logger(label: "test")
+        logger.logLevel = .error
+        logger.error(Dummy())
+
+        logging.history.assertExist(level: .error, message: "errorDescription")
+    }
+}
+
+extension Logger {
+    public func error(_ error: Error,
+                      metadata: @autoclosure () -> Logger.Metadata? = nil,
+                      file: String = #file, function: String = #function, line: UInt = #line) {
+        self.error("\(error.localizedDescription)", metadata: metadata(), file: file, function: function, line: line)
+    }
 }
