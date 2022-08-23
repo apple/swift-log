4ef24e1d65f64d7daa738168c4aeb818d1f9ca4a
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-08-23 17:43:05 +0900

    ignore package.resolved

--- scripts/preview_docc.sh
+++ scripts/preview_docc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
-## This source file is part of the Swift Distributed Actors open source project
+## This source file is part of the Swift Logging API open source project
 ##
-## Copyright (c) 2018-2019 Apple Inc. and the Swift Distributed Actors project authors
+## Copyright (c) 2018-2019 Apple Inc. and the Swift Logging API project authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.md for the list of Swift Distributed Actors project authors
+## See CONTRIBUTORS.md for the list of Swift Logging API project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
 ##===----------------------------------------------------------------------===##
 
-xcrun swift package --disable-sandbox preview-documentation --target DistributedActors
+xcrun swift package --disable-sandbox preview-documentation --target Logging
