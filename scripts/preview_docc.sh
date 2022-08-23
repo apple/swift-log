a8e1148aa548cf66a619b172082c033f069b8c46
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-08-23 18:29:56 +0900

    =docc Move to multiple Package.swift files (#231)
    * +docc prepare for docc docs
    
    * fix syntax in one of the package files
    
    * ignore package.resolved

---
+++ scripts/preview_docc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift Logging API open source project
+##
+## Copyright (c) 2018-2019 Apple Inc. and the Swift Logging API project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.md for the list of Swift Logging API project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+xcrun swift package --disable-sandbox preview-documentation --target Logging
