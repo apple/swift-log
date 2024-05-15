04d26b7af8eae7f73071d2b08d71fdf070614719
Gus Cairo <gus.cairo@apple.com>
2024-05-15 16:20:17 +0100

    More formatting

--- scripts/preview_docc.sh
+++ scripts/preview_docc.sh
@@ -7,7 +7,7 @@
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.md for the list of Swift Logging API project authors
+## See CONTRIBUTORS.txt for the list of Swift Logging API project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
