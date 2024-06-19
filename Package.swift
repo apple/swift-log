7adcf9aa155e45eab17633d681a4f57a1ea890a7
Peter Adams <63288215+PeterAdams-A@users.noreply.github.com>
2024-06-19 09:27:12 +0100

    Remove Docc plugin which is no longer required. (#311)
    Motivation:
    
    Plugin is no longer required to produce docs.
    There are now warnings in the plugin when included
    and it will delay build a small amount.
    
    Modifications:
    
    Remove plugin dependency.
    
    Result:
    
    No dependencies.

--- Package.swift
+++ Package.swift
@@ -24,9 +24,6 @@ let package = Package(
     products: [
         .library(name: "Logging", targets: ["Logging"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "Logging",
