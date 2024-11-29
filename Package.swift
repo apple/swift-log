fd55ed32709f0f668b3fd655c4b0ea914c71858a
Honza Dvorsky <honza@apple.com>
2024-11-29 17:14:08 +0100

    Enable strict concurrency

--- Package.swift
+++ Package.swift
@@ -31,3 +31,9 @@ let package = Package(
         ),
     ]
 )
+
+for target in package.targets {
+    var settings = target.swiftSettings ?? []
+    settings.append(.enableExperimentalFeature("StrictConcurrency=complete"))
+    target.swiftSettings = settings
+}
