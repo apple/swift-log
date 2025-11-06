a557fb404c90ac303221bb43d1e05d5146e7549e
Vladimir Kukushkin <kukushechkin@mac.com>
2025-11-06 14:43:09 +0000

    poke CI

--- Benchmarks/Benchmarks/SwiftLogBenchmarks/SwiftLogBenchmarks.swift
+++ Benchmarks/Benchmarks/SwiftLogBenchmarks/SwiftLogBenchmarks.swift
@@ -21,7 +21,7 @@ func nothingFunc() {
 }
 
 let benchmarks: @Sendable () -> Void = {
-    let iterations = 1 // poke CI poke
+    let iterations = 1 // poke CI
     let metrics: [BenchmarkMetric] = [.instructions, .objectAllocCount]
 
     Benchmark(
