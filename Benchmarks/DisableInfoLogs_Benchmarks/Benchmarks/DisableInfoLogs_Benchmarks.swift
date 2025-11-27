a04cc45b45ae72b5bded00fbec97eed4560cfe1e
Vladimir Kukushkin <kukushechkin@mac.com>
2025-11-27 17:33:53 +0000

    This is an abomination, and it doesn’t work :(

---
+++ Benchmarks/DisableInfoLogs_Benchmarks/Benchmarks/DisableInfoLogs_Benchmarks.swift
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Logging API open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift Logging API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Logging API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Benchmark
+import BenchmarkingUtils
+import Foundation
+import Logging
+
+public let benchmarks: @Sendable () -> Void = {
+    makeBenchmark(loggerLevel: .info, logLevel: .info, "_DisableInfoLogs") { logger in
+        logger.info("hello, benchmarking world")
+    }
+}
--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -9,26 +9,33 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(
-            path: "../",
-            traits: [
-                "DisableTraceLogs", "DisableDebugLogs", "DisableInfoLogs", "DisableNoticeLogs", "DisableWarningLogs",
-                // "DisableErrorLogs", "DisableCriticalLogs",
-            ]
-        ),
         .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.29.6"),
+        .package(path: "NoTraits_Benchmarks"),
+        .package(path: "DisableTraceLogs_Benchmarks"),
+        .package(path: "DisableDebugLogs_Benchmarks"),
+        .package(path: "DisableInfoLogs_Benchmarks"),
+        .package(path: "DisableNoticeLogs_Benchmarks"),
+        .package(path: "DisableWarningLogs_Benchmarks"),
+        .package(path: "DisableErrorLogs_Benchmarks"),
+        .package(path: "DisableCriticalLogs_Benchmarks"),
     ]
 )
 
-// Benchmark of SwiftLogBenchmarks
 package.targets += [
     .executableTarget(
-        name: "SwiftLogBenchmarks",
+        name: "Benchmarks",
         dependencies: [
             .product(name: "Benchmark", package: "package-benchmark"),
-            .product(name: "Logging", package: "swift-log"),
+            .product(name: "NoTraits_Benchmarks", package: "NoTraits_Benchmarks"),
+            .product(name: "DisableTraceLogs_Benchmarks", package: "DisableTraceLogs_Benchmarks"),
+            .product(name: "DisableDebugLogs_Benchmarks", package: "DisableDebugLogs_Benchmarks"),
+            .product(name: "DisableInfoLogs_Benchmarks", package: "DisableInfoLogs_Benchmarks"),
+            .product(name: "DisableNoticeLogs_Benchmarks", package: "DisableNoticeLogs_Benchmarks"),
+            .product(name: "DisableWarningLogs_Benchmarks", package: "DisableWarningLogs_Benchmarks"),
+            .product(name: "DisableErrorLogs_Benchmarks", package: "DisableErrorLogs_Benchmarks"),
+            .product(name: "DisableCriticalLogs_Benchmarks", package: "DisableCriticalLogs_Benchmarks"),
         ],
-        path: "Benchmarks/SwiftLogBenchmarks",
+        path: "Benchmarks",
         plugins: [
             .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
         ]
