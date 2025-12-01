381c68add92de60d82e449d31f0a3b801f0a1102
Vladimir Kukushkin <kukushechkin@mac.com>
2025-12-01 10:17:46 +0000

    fixup! simplify implementation

--- Benchmarks/Sources/BenchmarksFactory/MakeBenchmark.swift
+++ Benchmarks/Sources/BenchmarksFactory/MakeBenchmark.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Logging API open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift Logging API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Logging API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Benchmark
 import Foundation
 import Logging
