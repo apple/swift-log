63eb1ab036454d28f5bb91c987d7425fc7ffe4c1
Vladimir Kukushkin <kukushechkin@mac.com>
2025-12-01 12:56:25 +0000

    switch from DisableXXXLogs to MaxLogLevelXXX traits

---
+++ Benchmarks/MaxLogLevelWarning/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MaxLogLevelWarning",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "MaxLogLevelWarning", targets: ["MaxLogLevelWarning"])
+    ],
+    dependencies: [
+        // swift-log
+        .package(
+            path: "../../",
+            traits: ["MaxLogLevelWarning"]
+        ),
+        // Parent Benchmarks
+        .package(name: "Benchmarks", path: "../"),
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.29.6"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "MaxLogLevelWarning",
+            dependencies: [
+                .product(name: "BenchmarksFactory", package: "Benchmarks"),
+                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Logging", package: "swift-log"),
+            ],
+            path: "Benchmarks/MaxLogLevelWarningBenchmarks",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        )
+    ]
+)
