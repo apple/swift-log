f38a10753a8b1bfe1727587998f2590556dfde6a
Vladimir Kukushkin <kukushechkin@mac.com>
2026-02-03 17:25:19 +0000

    remove os requirements

--- Package.swift
+++ Package.swift
@@ -17,9 +17,6 @@ import PackageDescription
 
 let package = Package(
     name: "swift-log",
-    platforms: [
-        .macOS(.v10_14), .iOS(.v12), .watchOS(.v5), .tvOS(.v12),
-    ],
     products: [
         .library(name: "Logging", targets: ["Logging"]),
         .library(name: "InMemoryLogging", targets: ["InMemoryLogging"]),
