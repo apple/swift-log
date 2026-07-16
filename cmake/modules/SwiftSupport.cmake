72b7aed885568703dcccfca9ccbdd0f78e0f4ddb
mitras-git <myemail.aditya@gmail.com>
2026-07-16 08:09:41 -0400

    add license headers and set BUILD_TESTING to OFF by default

--- cmake/modules/SwiftSupport.cmake
+++ cmake/modules/SwiftSupport.cmake
@@ -1,4 +1,18 @@
 
+#===----------------------------------------------------------------------===#
+#
+# This source file is part of the Swift Logging API open source project
+#
+# Copyright (c) 2018-2026 Apple Inc. and the Swift Logging API project authors
+# Licensed under Apache License v2.0
+#
+# See LICENSE.txt for license information
+# See CONTRIBUTORS.txt for the list of Swift Logging API project authors
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#===----------------------------------------------------------------------===#
+
 # Returns the os name in a variable
 #
 # Usage:
