cf3e828428b880c744fddb663c1bce7bf51370d9
mitra <myemail.aditya@gmail.com>
2026-09-17 12:31:04 -0400

    Remove invalid exclude statements from testTarget (#514)
    I removed the stale exclude statements from `Package.swift` and
    `Package@swift-6.1.swift`
    
    ### Motivation:
    
    In #485, CMake build support was added, as part of it, 99c554b added
    `exclude: ["CMakeLists.txt"]` to all four SwiftPM targets, including
    tests, but the CI was revealing some issues with CMake test-targets. So
    after review, in 4a81108 the CMake build files were removed from the
    `/Tests` directory but I accidentally left behind the exclusions in the
    package manifests.
    
    ### Modifications:
    
    Removed the exclude statements from Package.swift and
    Package@swift-6.1.swift.
    
    ### Result:
    
    No SwiftPM errors!
    
    Fixes: #513

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -39,13 +39,11 @@ let package = Package(
         ),
         .testTarget(
             name: "LoggingTests",
-            dependencies: ["Logging"],
-            exclude: ["CMakeLists.txt"]
+            dependencies: ["Logging"]
         ),
         .testTarget(
             name: "InMemoryLoggingTests",
-            dependencies: ["InMemoryLogging", "Logging"],
-            exclude: ["CMakeLists.txt"]
+            dependencies: ["InMemoryLogging", "Logging"]
         ),
     ]
 )
